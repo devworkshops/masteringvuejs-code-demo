26f8530d66fa53f12cbee5271221914abc78101a
Thiago Passos <thiago@passos.com.au>
2019-01-03 21:17:09 +1000

    adding default vue.config.js

---
+++ beyond-the-basics/vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  outputDir: undefined,
+  baseUrl: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+}
