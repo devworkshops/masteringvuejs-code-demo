95b29c1142c99fb9c893c8b22cf98a1e17cb660f
Thiago Passos <thiagospassos@gmail.com>
2019-01-07 09:42:54 +1000

    beyond the basics clean up for step 0

--- beyond-the-basics/vue.config.js
+++ beyond-the-basics/vue.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  outputDir: undefined,
-  baseUrl: undefined,
-  assetsDir: undefined,
-  runtimeCompiler: undefined,
-  productionSourceMap: undefined,
-  parallel: undefined,
-  css: undefined,
-  configureWebpack: {
-    devtool: 'source-map'
-  }
+    outputDir: undefined,
+    baseUrl: undefined,
+    assetsDir: undefined,
+    runtimeCompiler: undefined,
+    productionSourceMap: undefined,
+    parallel: undefined,
+    css: undefined,
+    configureWebpack: {
+        devtool: 'source-map'
+    }
 }
