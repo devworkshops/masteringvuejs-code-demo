ed09e1c95abdbc69c7d6c9baacdf36d3a038ff45
Thiago Passos <thiagospassos@gmail.com>
2019-01-09 11:33:53 +1000

    creating testing folder, or step 0

---
+++ testing/src/services/NorthwindService.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+    baseURL: `//localhost:3000`,
+    withCredentials: false, // This is the default
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+    }
+})
+
+export const SupplierService = {
+    getSuppliers() {
+        return apiClient.get('/suppliers')
+    },
+    getSupplier(id) {
+        return apiClient.get('/suppliers/' + id)
+    },
+    updateSupplier(supplier) {
+        return apiClient.put('/suppliers/' + supplier.id, supplier);
+    }
+}
